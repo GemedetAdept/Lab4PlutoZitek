a02132d575d7faa7defdda3a4249f8f953a7bdc1
GemedetAdept <vivian.zitek@gmail.com>
2022-10-05 21:13:50 -0600

    Implement unique name generation.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// 2022-09-29; CS 1400; Lab 4
 
 // Calculate yearly salary from inputted values
-static decimal yearlySalary(decimal hourlyWage, int hoursPerWeek, int weeksPerYear) {
+decimal yearlySalary(decimal hourlyWage, int hoursPerWeek, int weeksPerYear) {
 
 	decimal yearlySalaryValue = 0M;
 	yearlySalaryValue = ((decimal)hoursPerWeek * (decimal)weeksPerYear) * hourlyWage;
@@ -10,7 +10,7 @@ static decimal yearlySalary(decimal hourlyWage, int hoursPerWeek, int weeksPerYe
 }
 
 // Rectify generated integer wages into decimal values
-static decimal rectifyWage(int integerWage){
+decimal rectifyWage(int integerWage){
 
 	decimal rectifiedWage = 0.0M;
 	rectifiedWage = (decimal)integerWage / 100.00M;
@@ -18,15 +18,11 @@ static decimal rectifyWage(int integerWage){
 	return rectifiedWage;
 }
 
-
 int numberOfPeople = 1000;
 int wageLowerBound = 725;
 int wageUpperBound = 4000;
 int hoursPerWeek = 40;
 int weeksPerYear = 50;
-
-// decimal salaryValue = 0M;
-// salaryValue = yearlySalary(hourlyWage, hoursPerWeek, weeksPerYear);
 
 // Generate integer wages and store in array
 int[] integerWages = new int[numberOfPeople];
@@ -60,13 +56,70 @@ for (int i = 0; i < numberOfPeople; i++) {
 	yearlySalaries[i] = yearlySalaryValue;
 }
 
+// --------------------------------------------------------------
+
+// Generate array of CV pairs from lists of consonants and vowels
+string[] consonants = new string[] {
+
+	"b", "ch", "d", "f", "g", "h", "k", "l", "m", "n", "\u00F1", "ng", "'", "p", "r", "s", "t", "y",
+};
+
+string[] vowels = new string[] {
+
+	"a", "\u00E5", "e", "i", "o", "u",
+};
+
+// Dynamically find the needed array length
+int pairsLength = consonants.Length * vowels.Length;
+string[] pairsCV = new string[pairsLength];
+
+// For each consonant, loop through each vowel and combine
+int pairIndex = 0;
+for (int i = 0; i < consonants.Length; i++) {
+
+	string combined = "";
+
+	foreach (string vowel in vowels) {
+		combined = consonants[i] + vowel;
+		pairsCV[pairIndex] = combined;
+		pairIndex += 1;
+	}
+}
+
+// --------------------------------------------------------------
+
+// Generate array of names / CV-pair combinations
+int namesLength = pairsLength * pairsLength;
+Console.WriteLine($"nameLength = {namesLength}");
+string[] namesArray = new string[namesLength];
+Console.WriteLine($"namesArray = {namesArray.Length}");
+
+int nameIndex = 0;
+for (int i = 0; i < pairsCV.Length; i++) {
+
+	string combined = "";
+
+	foreach (string nameBeta in pairsCV) {
+
+		combined = pairsCV[i] + nameBeta;
+
+			if (combined == " ") {
+				break;
+			}
+
+		namesArray[nameIndex] = combined;
+		nameIndex += 1;
+
+	}
+}
+
+// --------------------------------------------------------------
 
 // Output Printing
 for (int i = 0; i < numberOfPeople; i++) {
 
-	Console.WriteLine($"${rectifiedWages[i]:00.00}/hr === ${yearlySalaries[i]:0.00}/yr");
+	// Console.WriteLine($"${rectifiedWages[i]:00.00}/hr === ${yearlySalaries[i]:0.00}/yr");
 }
-
 
 // Addendum Error Log
 Console.WriteLine("\n");
